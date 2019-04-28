699117dd43b8641e9d0f26fd24f360052c820571
Hanene1 <47008546+Hanene1@users.noreply.github.com>
2019-04-28 17:34:52 +0100

    Add files via upload

--- entities/livraison.php
+++ entities/livraison.php
@@ -2,19 +2,22 @@
 class Livraison {
 	private $id_livraison;
 	private $id_livreur;
-	private $id_commande;
 	private $adresse;
 	private $ville;
 	private $code_postal;
+	private $date_livraison;
+	private $etat;
 
-	function __construct($id_livraison,$id_livreur,$id_commande,$adresse,$ville,$code_postal)
+	function __construct($id_livraison,$id_livreur,$adresse,$ville,$code_postal,$date_livraison,$etat)
 	{
 		$this->id_livraison=$id_livraison;
 		$this->id_livreur=$id_livreur;
-		$this->id_commande=$id_commande;
 		$this->ville=$ville;
 		$this->adresse=$adresse;
 		$this->code_postal=$code_postal;
+		$this->date_livraison=$date_livraison;
+		$this->etat=$etat;
+
 	}
 	
 	function getID(){
@@ -26,14 +29,17 @@ class Livraison {
 	function getAdresse(){
 		return $this->adresse;
 	}
-	function getCommande(){
-		return $this->id_commande;
-	}
 	function getVille(){
 		return $this->ville;
 	}
 	function getCode(){
 		return $this->code_postal;
+	}
+	function getDate(){
+		return $this->date_livraison;
+	}
+	function getEtat(){
+		return $this->etat;
 	}
 
 	function setLivreur($id_livreur){
@@ -42,14 +48,17 @@ class Livraison {
 	function setAdresse($adresse){
 		$this->adresse;
 	}
-	function setCommande($id_commande){
-		$this->id_commande=$id_commande;
-	}
 	function setVille($ville){
 		$this->ville=$ville;
 	}
 	function setCode($code_postal){
 	    $this->code_postal=$code_postal;
+	}
+	function setDate($date_livraison){
+	    $this->date_livraison=$date_livraison;
+	}
+	function setEtat($etat){
+	    $this->etat=$etat;
 	}
 	
 }
